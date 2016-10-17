e492d277685b6d452e2a2e719aaf1b4398f8b0ca
IceGroup <wen007@msn.com>
2016-10-17 21:09:54 +0800

    创建AdPlayer

--- app/src/main/java/com/icegroup/adclient/client/LoginActivity.java
+++ app/src/main/java/com/icegroup/adclient/client/LoginActivity.java
@@ -3,6 +3,7 @@ package com.icegroup.adclient.client;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -35,27 +36,31 @@ import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
+ *  一个登录屏幕,提供通过电子邮件/密码登录。
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
     /**
      * Id to identity READ_CONTACTS permission request.
+     * Id来标识READ_CONTACTS请求许可。
      */
     private static final int REQUEST_READ_CONTACTS = 0;
 
     /**
      * A dummy authentication store containing known user names and passwords.
+     * 一个虚拟身份验证存储包含用户名和密码。
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+            "foo@example.com:hello", "bar@example.com:world","icegroup@163.com:wenwen"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
+     * 记录登录的任务,以确保如果请求,我们可以取消它。
      */
     private UserLoginTask mAuthTask = null;
 
-    // UI references.
+    // UI references.UI 引用.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
     private View mProgressView;
@@ -65,7 +70,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // Set up the login form.
+        // Set up the login form.设置登录表单
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
 
@@ -125,6 +130,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     /**
      * Callback received when a permissions request has been completed.
+     * 回调收到权限请求时已经完成。
      */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
@@ -138,34 +144,33 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
 
     /**
-     * Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
+     * Attempts to sign in or register the account specified by the login form.If there are form errors (invalid email, missing fields, etc.), the errors are presented and no actual login attempt is made.
+     *试图登录或注册帐户指定的登录表单。如果有错误(无效的电子邮件,缺少字段等),并给出了错误,没有实际的登录尝试。*
      */
     private void attemptLogin() {
         if (mAuthTask != null) {
             return;
         }
 
-        // Reset errors.
+        // Reset errors.重置错误
         mEmailView.setError(null);
         mPasswordView.setError(null);
 
-        // Store values at the time of the login attempt.
+        // Store values at the time of the login attempt.存储值时的登录尝试。
         String email = mEmailView.getText().toString();
         String password = mPasswordView.getText().toString();
 
         boolean cancel = false;
         View focusView = null;
 
-        // Check for a valid password, if the user entered one.
+        // Check for a valid password, if the user entered one.检查有效的密码,如果用户输入一个。
         if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
             mPasswordView.setError(getString(R.string.error_invalid_password));
             focusView = mPasswordView;
             cancel = true;
         }
 
-        // Check for a valid email address.
+        // Check for a valid email address.检查一个有效的电子邮件地址。
         if (TextUtils.isEmpty(email)) {
             mEmailView.setError(getString(R.string.error_field_required));
             focusView = mEmailView;
@@ -177,15 +182,16 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         }
 
         if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
+            // There was an error; don't attempt login and focus the first form field with an error.
+            // 有一个错误,不要尝试登录和关注第一个表单字段与一个错误。
             focusView.requestFocus();
         } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
+            // Show a progress spinner, and kick off a background task to perform the user login attempt.
+            // 显示一个进度转轮,启动一个后台任务执行用户的登录尝试。
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+
         }
     }
     private boolean isEmailValid(String email) {
@@ -203,9 +209,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
     private void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow for very easy animations. If available, use these APIs to fade-in the progress spinner.
+        // 在蜂窝MR2 ViewPropertyAnimator api,允许非常简单的动画。如果可用,使用这些api淡入微调控制项的进展。
+        //
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
             int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
@@ -237,17 +243,17 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
         return new CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
+                // Retrieve data rows for the device user's 'profile' contact.检索数据行设备用户的“概要”接触。
                 Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
                         ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
 
-                // Select only email addresses.
+                // Select only email addresses.选择唯一的电子邮件地址。
                 ContactsContract.Contacts.Data.MIMETYPE +
                         " = ?", new String[]{ContactsContract.CommonDataKinds.Email
                                                                      .CONTENT_ITEM_TYPE},
 
-                // Show primary email addresses first. Note that there won't be
-                // a primary email address if the user hasn't specified one.
+                // Show primary email addresses first. Note that there won't be a primary email address if the user hasn't specified one.
+                // 显示主电子邮件地址。注意,不会有一个主电子邮件地址如果用户没有指定一个。
                 ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
     }
 
@@ -332,6 +338,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
+                //进入设置页面
+                Intent intent =  new Intent();
+                //setClass函数的第一个参数是一个Context对象.
+                //Context是一个类,Activity是Context
+                intent.setClass(LoginActivity.this,SettingsActivity.class);
+                startActivity(intent);
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
